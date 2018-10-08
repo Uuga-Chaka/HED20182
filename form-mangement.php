02711dc5e9de3248f24e76d163446e4e435259ef
Uuga-chaka <jpgarias09@gmail.com>
2018-10-08 07:49:32 -0500

    pruebita de amor

--- form-mangement.php
+++ form-mangement.php
@@ -30,7 +30,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         } 
     }
     if(empty($_POST["msj"])){
-        $msj_error = $_POST["msj"]);
+        $msj_error = "Falta incluir el mensaje";
     }else{
         $msj= test_input($_POST["msj"]);
     }
